045d8291102edf76fcd9d1361f6eb92c29908917
erdemmgunal <65365648+erdemmgunal@users.noreply.github.com>
2024-05-08 19:45:18 +0300

    chore: Refactor middleware and update Discord webhook for tracking page visits

--- src/middleware.jsx
+++ src/middleware.jsx
@@ -1,18 +1,13 @@
 import { NextResponse } from "next/server";
 
 export async function middleware(request) {
-    if (request.nextUrl.pathname.startsWith("/r/")) {
-        const id = request.nextUrl.pathname.replace("/r/", "");
-
-        const webhook = 'https://discord.com/api/webhooks/1144326393056989348/5DWeiOX9IMR_SQlBJWEH8erBl09Aoiu2xVI7jkwjg5Hm3Z-w5eUx5qjkQQHYWYMETijm';
-        const response = await fetch(webhook, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ 'content': `${id} ${request.geo?.country}` })
-        });
-    }
+    const response = await fetch(process.env.DISCORD_WEBHOOK, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ 'content': `${request.nextUrl.pathname} ${request.geo?.country}` })
+    });
     return NextResponse.next();
 }
 
