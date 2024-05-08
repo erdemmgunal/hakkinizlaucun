bd24204b22f1e1bf172e140d02ef5564addc3fb5
erdemmgunal <65365648+erdemmgunal@users.noreply.github.com>
2024-05-08 19:12:51 +0300

    chore: Add Discord webhook for tracking page visits

--- src/app/api/r/[id]/route.js
+++ src/app/api/r/[id]/route.js
@@ -7,6 +7,5 @@ export async function GET(request, { params }) {
     // return NextResponse.redirect(`https://www.hakkiscanner.com/flights/${id}`);
 }
 
-
 // localhost:3000/api/r/422caa377e334df5bd225b647ed65e52
 // https://web.flypgs.com/booking?language=tr&adultCount=1&arrivalPort=OTP&departurePort=SAW&currency=TRY&dateOption=1&departureDate=2024-11-28&returnDate=2024-12-02
--- src/app/middleware.jsx
+++ src/app/middleware.jsx
@@ -3,7 +3,16 @@ import { NextResponse } from "next/server";
 export async function middleware(request) {
     console.log(request.nextUrl.pathname);
     if (req.nextUrl.pathname === "/r/:id") {
-        console.log("TRACK!");
+        const content = 'Hello, World!'
+        const webhook = 'https://discord.com/api/webhooks/1144326393056989348/5DWeiOX9IMR_SQlBJWEH8erBl09Aoiu2xVI7jkwjg5Hm3Z-w5eUx5qjkQQHYWYMETijm';
+        const response = await fetch(webhook, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ 'content': `${request.nextUrl.pathname} ${request.geo?.country}` })
+        });
+        return NextResponse.next();
     }
 }
 
