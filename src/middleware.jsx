baab6f1ab142358df0ee8a705f7d46258c2b2ce0
erdemmgunal <65365648+erdemmgunal@users.noreply.github.com>
2024-05-08 19:53:53 +0300

    Refactor middleware to include embed data for Discord webhook

--- src/middleware.jsx
+++ src/middleware.jsx
@@ -1,12 +1,26 @@
 import { NextResponse } from "next/server";
 
 export async function middleware(request) {
+    const embedData = {
+        embeds: [
+            {
+                title: 'Log Information',
+                color: 0x0099ff,
+                fields: [
+                    { name: 'Path', value: `${request.nextUrl.pathname}` },
+                    { name: 'Country', value: `${request.geo?.countr}` },
+                    { name: 'Timestamp', value: new Date().toLocaleString() }
+                ]
+            }
+        ]
+    };
+    
     const response = await fetch(process.env.DISCORD_WEBHOOK, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ 'content': `${request.nextUrl.pathname} ${request.geo?.country}` })
+        body: JSON.stringify(embedData)
     });
     return NextResponse.next();
 }
